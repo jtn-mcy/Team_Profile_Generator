2583f3aceda883716bd1034ec43e0908e610a23e
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-16 21:30:49 -0700

    add getters to classes.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const fm = require('fs');
+// const fm = require('fs');
 const inquirer = require('inquirer');
-const prompt = require('./src/promptQuestions.js')
-const generate = require('./src/helperFunctions.js')
-const classes = require('./lib/classes.js')
+const qPrompt = require('./src/promptQuestions.js')
+// const generate = require('./src/helperFunctions.js')
+// const classes = require('./lib/classes.js')
 
 //Prompt the user a series of questions to create their team
 //
-
-console.log(classes);
+inquirer
+    .prompt(
+        qPrompt.promptInit,
+        qPrompt.promptManager
+    )
+    .then((response) => {
+        console.log(response);
+    })
