001d5aa70176834e0f04a06cdc3a6f53204126a4
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-17 14:21:26 -0700

    add throws to classes.js, complete tests for initialization employee creation

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,9 +4,49 @@ const { Employee } = require('../lib/classes.js')
 describe("Employee", () => {
     describe("Initialization", () => {
         it("should create an object with a name, id, and email with associated class methods getName, getID, getEmail", () => {
-        const employee = new Employee('Kenny', '01', "kenny@test.com")
+        const employee = new Employee('Kenny', 1, "kenny@test.com")
 
-        expect(employee).toMatchObject({name: 'Kenny', id: '01', email: 'kenny@test.com', getName: expect.any(Function), getId: expect.any(Function), getEmail: expect.any(Function)});
-       });
-    })
-})
+        expect(employee).toMatchObject({
+            name: 'Kenny', 
+            id: 1, 
+            email: 'kenny@test.com', 
+            // getName: expect.any(Function), 
+            // getId: expect.any(Function), 
+            // getEmail: expect.any(Function)
+            });
+        });
+        // * NO ARGUMENT ERROR
+        it ("should throw an error if provided no arguments", () => {
+            const cb = () => new Employee();
+            expect(cb).toThrow();
+        });
+        // * NAME ERRORS
+        it("should throw an error if invalid name", () => {
+           const cb = () => new Employee('  ', 1, 'kenny@test.com');
+           const err = new Error("Expected parameter 'name' to be a non-empty string");
+            expect(cb).toThrowError(err);
+        });
+        // * NUMBER ERRORS
+        it("should throw an error if negative number", () => {
+            const cb = () => new Employee('Kenny', -1, 'kenny@test.com');
+            const err = new Error("Expected parameter 'id' to be a non-negative number");
+             expect(cb).toThrowError(err);
+        });
+        it("should throw an error if no number", () => {
+            const cb = () => new Employee('Kenny', '', 'kenny@test.com');
+            const err = new Error("Expected parameter 'id' to be a non-negative number");
+             expect(cb).toThrowError(err);
+        });
+        it("should throw an error if invalid string as a number", () => {
+            const cb = () => new Employee('Kenny', 'hello', 'kenny@test.com');
+            const err = new Error("Expected parameter 'id' to be a non-negative number");
+             expect(cb).toThrowError(err);
+        });
+        // * EMAIL ERRORS
+        it("should throw an error if invalid email", () => {
+            const cb = () => new Employee('Kenny', 1, 'kennytest.com');
+            const err = new Error('Must enter a valid email');
+             expect(cb).toThrowError(err);
+        });
+    });
+});
