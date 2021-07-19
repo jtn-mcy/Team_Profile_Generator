9d65e14abd5d994cb5adedfd406692653ff78fff
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-18 18:41:08 -0700

    modify generateHTML function

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,27 +1,4 @@
-//Generate HTML
-generateTeamHTML = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${teamName}</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
-    <div class="jumbotron jumbotron-fluid text-center p-5 bg-info bg-gradient">
-        <h1 class="display-3 text-white">My Team</h1>
-    </div>
 
-    <div class='container'>
-        <div class = 'p-3 row flex-wrap' id='employeeCard'>
-        </div>  
-    </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-</body>
-</html>
-`
 
 // Generate Cards
 //Employee card
@@ -44,7 +21,31 @@ cardTemplate =
     </div>
 </div>`
 
-const generateHTML = () => {
+const generateHTML = (templObj) => {
+    //Generate HTML
+    generateTeamHTML = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>${templObj.teamName}</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid text-center p-5 bg-info bg-gradient">
+            <h1 class="display-3 text-white">My Team</h1>
+        </div>
+
+        <div class='container'>
+            <div class = 'p-3 row flex-wrap' id='employeeCard'>
+            </div>  
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+    </html>
+    `
     return generateTeamHTML
 }
 
